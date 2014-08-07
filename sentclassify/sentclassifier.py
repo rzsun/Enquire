5df666cc82d5b0468bdbe4f4e4d58b30bfc37170
Zheng Sun <zhensn@gmail.com>
2014-08-07 13:10:20 -0700

    tweetgetter and sentclassifier

--- sentclassify/sentclassifier.py
+++ sentclassify/sentclassifier.py
@@ -2,36 +2,38 @@ import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
 import pickle
-import csv
 
 class sentClassifier:
     # creates a classifier from the movie review corpus
-    def __init__(self):
-        negids = movie_reviews.fileids('neg')
-        posids = movie_reviews.fileids('pos')
-        negfeats = [(self.word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in movie_reviews.fileids('neg')]
-        posfeats = [(self.word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in movie_reviews.fileids('pos')]
-        trainfeats = negfeats + posfeats
-        self.classifier = NaiveBayesClassifier.train(trainfeats)
+    def __init__(self, load = False, loadFile = ""):
+        if(load):
+            self.loadClassifier(loadFile)
+        else:
+            negids = movie_reviews.fileids('neg')
+            posids = movie_reviews.fileids('pos')
+            negfeats = [(self.word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in movie_reviews.fileids('neg')]
+            posfeats = [(self.word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in movie_reviews.fileids('pos')]
+            trainfeats = negfeats + posfeats
+            self.classifier = NaiveBayesClassifier.train(trainfeats)
     
     # classifies a string    
-    def __call__(self, feedbackString):
-        return self.classifier.classify(self.word_feats(feedbackString.split()))
+    def __call__(self, tweetString):
+        return self.classifier.classify(self.word_feats(tweetString.split()))
     
     def showMostInformativeFeatures(self, n):
         self.classifier.show_most_informative_features(n)
     
     # extracts features from a list of words
     def word_feats(self, words):
-        return dict([(word, True) for word in words])
+        return dict([(word.lower(), True) for word in words])
     
     # saves the classifier to a binary file    
     def saveClassifier(self, filename):
         f = open(filename, 'wb')
-        pickle.dump(self, f)
+        pickle.dump(self.classifier, f)
         f.close()
         
     def loadClassifier(self, filename):
-        f = open('classifier.pickle', 'rb')
-        self = pickle.load(f)
+        f = open(filename, 'rb')
+        self.classifier = pickle.load(f)
         f.close()
--- sentclassify/tweetgetter.py
+++ sentclassify/tweetgetter.py
@@ -2,21 +2,27 @@ import tweepy
 import json
 import time
 
-# Authentication details. To  obtain these visit dev.twitter.com
-consumer_key = '6F86k5I5SlRJhdCe2sPNIq5Lf'
-consumer_secret = 'fgRScFMqnXzEdsRPWWD26eq4AaN0RNNQQA6aE1NS1cWsJchu95'
-access_token = '989863279-pbCEbSGxlwpPdHj8LAHbjSnQg7FWCyqssosbI37f'
-access_token_secret = 'kWSENixo2ar4w21REKMhNW7zPiFCPUBY2e2gdWiiE3oAj'
+def getTweets(searchTerm):
+	# Authentication details. To  obtain these visit dev.twitter.com
+	consumer_key = '6F86k5I5SlRJhdCe2sPNIq5Lf'
+	consumer_secret = 'fgRScFMqnXzEdsRPWWD26eq4AaN0RNNQQA6aE1NS1cWsJchu95'
+	access_token = '989863279-pbCEbSGxlwpPdHj8LAHbjSnQg7FWCyqssosbI37f'
+	access_token_secret = 'kWSENixo2ar4w21REKMhNW7zPiFCPUBY2e2gdWiiE3oAj'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search,
-                           q="google",
-                           rpp=100,
-                           result_type="recent",
-                           include_entities=True,
-                           lang="en").items(5):
-    print tweet.created_at, tweet.text
+	api = tweepy.API(auth)
+	results = []
+	for t in tweepy.Cursor(api.search,
+		                       q=searchTerm,
+		                       rpp=100,
+		                       result_type="recent",
+		                       include_entities=True,
+		                       lang="en").items(10):
+		tweetDict = {}
+		tweetDict["time"] = t.created_at
+		tweetDict["text"] = t.text
+		results.append(tweetDict)
+	return results
 
